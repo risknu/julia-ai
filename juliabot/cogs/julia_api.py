dfb4fd581d3fcfaca93c05adc83a13061dd87bdc
Mak Moskalenko <145287544+risknu@users.noreply.github.com>
2023-12-16 13:50:45 +0100

    Added storage scripts
    Added storage script and fixed bugs in firnetwork method.

--- juliabot/cogs/julia_api.py
+++ juliabot/cogs/julia_api.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 import asyncio
+import sqlite3
+
+from os import path, PathLike
+from typing import Union
 
 import disnake
 from disnake.ext import commands
@@ -9,21 +13,42 @@ from juliaai import JuliaAIAPI
 from juliaai.misc import MetaSettings
 meta_settings: MetaSettings = MetaSettings
 
+def get_file_size(file_path: Union[str, PathLike] = None) -> tuple:
+    if not file_path or not path.exists(file_path):
+        return 0, 0
+    size_in_bytes = path.getsize(file_path)
+    size_in_kb = size_in_bytes / 1024
+    return size_in_bytes, size_in_kb
+
 
 class JuliaAPI(commands.Cog):
     def __init__(self, bot: commands.Bot = None) -> None:
         self.bot: commands.Bot = bot
         self.julia_api: JuliaAIAPI = JuliaAIAPI()
 
-    @commands.slash_command(description="sizeof")
+    @commands.slash_command(description="You can specify the size of the database in megabytes (MB), gigabytes (GB), or kilobytes (KB)")
     async def sizeof(self, interaction: disnake.ApplicationCommandInteraction = None) -> None:
-        await interaction.send('sizeof')
+        await interaction.response.defer()
+        async with interaction.channel.typing():
+            await asyncio.sleep(1)
+        size_in_bytes, size_in_kb = get_file_size('db.sqlite3')
+        await interaction.edit_original_response(f'The server database weighs around `{size_in_bytes}B` or `{size_in_kb}KB`\nFor more commands, refer to our menu using `?help`')
 
-    @commands.slash_command(description="countof")
+    @commands.slash_command(description="You can specify the number of rows for training the neural network")
     async def countof(self, interaction: disnake.ApplicationCommandInteraction = None) -> None:
-        await interaction.send('countof')
+        await interaction.response.defer()
+        async with interaction.channel.typing():
+            await asyncio.sleep(1)
+        if not path.exists('db.sqlite3'):
+            return
+        connection = sqlite3.connect('db.sqlite3')
+        cursor = connection.cursor()
+        query: str = f"SELECT COUNT(*) FROM statement;"
+        cursor.execute(query)
+        row_count = cursor.fetchone()[0]
+        await interaction.edit_original_response(f'The amount of data for training the chatbot is `{row_count}`\nUse our help command `?help` for other commands')
 
-    @commands.slash_command(description="fitnetwork")
+    @commands.slash_command(description="You can train the neural network on your data that you can provide to us")
     async def fitnetwork(self, interaction: disnake.ApplicationCommandInteraction = None,
                          input_sentence: str = None,
                          output_sentence: str = None) -> None:
@@ -31,17 +56,17 @@ class JuliaAPI(commands.Cog):
         if not input_sentence or not output_sentence:
             async with interaction.channel.typing():
                 await asyncio.sleep(1)
-            interaction.edit_original_response(f'{interaction.user.mention}, You did not specify one of the parameters for the neural network settings. Please review them and correct your command\nUse the help menu with :: `?help`')
+            await interaction.edit_original_response(f'{interaction.user.mention}, You did not specify one of the parameters for the neural network settings. Please review them and correct your command\nUse the help menu with :: `?help`')
             return
         train_code: bool = self.julia_api.train(input_sentence, output_sentence)
         if not train_code:
             async with interaction.channel.typing():
                 await asyncio.sleep(1)
-            interaction.edit_original_response(f'{interaction.user.mention}, You did not specify one of the parameters for the neural network settings. Please review them and correct your command\nUse the help menu with :: `?help`')
+            await interaction.edit_original_response(f'{interaction.user.mention}, You did not specify one of the parameters for the neural network settings. Please review them and correct your command\nUse the help menu with :: `?help`')
             return
         async with interaction.channel.typing():
             await asyncio.sleep(1)
-        interaction.edit_original_response(f'{interaction.user.mention}, You have successfully trained the neural network to respond `{output_sentence}` to the input `{input_sentence}`\nFor more commands, use :: `?help`')
+        await interaction.edit_original_response(f'{interaction.user.mention}, You have successfully trained the neural network to respond `{output_sentence}` to the input `{input_sentence}`\nFor more commands, use :: `?help`')
     
 
 def setup(bot: commands.Bot = None) -> None:
